9b08af5dc2b0fbe333a9a442bbec0ac4c35f4b0c
Andrei Stoica <andrei@stoica.dev>
2024-04-07 13:25:54 +0100

    Add pin checking

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
+const CORRECT_PIN = '1234';
 
 const enterButton = document.getElementById('enter-button');
 console.log('enterButton: ', enterButton);
 enterButton?.addEventListener('click', () => {
-  console.log('You clicked the button!');
+    console.log('You clicked the enter button!');
+
+    const input = document.getElementById('pin-input');
+    console.log('input: ', input);
+
+    if (input instanceof HTMLInputElement) {   
+        const pin = input?.value;
+        console.log('pin: ', pin);
+
+        if (pin === CORRECT_PIN) {
+            console.log('Correct PIN entered!');
+        } else {
+            alert('Incorrect PIN entered!');
+        }
+    } else {
+        console.error('input is not an HTMLInputElement');
+    }
 })
