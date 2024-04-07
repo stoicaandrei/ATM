bf1489637cf0f02536ba9cc45f259fd29d645b63
Andrei Stoica <andrei@stoica.dev>
2024-04-07 12:51:21 +0100

    Add event listener on enter button

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('Hello World!')
+
+const enterButton = document.querySelector('#enter-button');
+
+if (enterButton) {
+    enterButton.addEventListener('click', () => {
+        console.log('click');
+    })
+}
